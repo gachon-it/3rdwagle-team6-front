77995333e06d4e5ae38735856ff8c9e2160b4e72
kim <jskim6335@naver.com>
2025-02-22 04:52:46 +0900

    edit basic location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Main from './pages/Main/Main'
 import Profile from './pages/Profile/Profile'
@@ -11,6 +11,7 @@ import './App.css'
 function App() {
   return (
     <Routes>
+      <Route path="" element={<Navigate to="/Login" replace />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Main" element={<Main />} />
       <Route path="/Message" element={<Message />} />
